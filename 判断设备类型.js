76ab27f50e0284d9084910633a394d4928c23f1c
MrBaiYinLuo <1019369380@qq.com>
2016-07-21 22:23:15 +0800

    移动端和PC端设备类型的判断

---
+++ 判断设备类型.js
@@ -0,0 +1,12 @@
+function IsPC(){
+	var userAgentInfo = navigator.userAgent;//使用navigator.userAgent来判断浏览器类型。  
+	var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");  
+	var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+       if (userAgentInfo.indexOf(Agents[v]) >= 0) {
+       	flag = false;
+       	break;
+       }
+    }
+   return flag;
+}
